f00013435018872e0ece30858c833abc34925ffa
KaneishaR <kaneisha.reese@gmail.com>
2021-04-29 15:30:49 -0500

    security

---
+++ src/main/java/com/kcodes/studygame/model/User.java
@@ -0,0 +1,32 @@
+package com.kcodes.studygame.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Table(name = "USER")
+@DiscriminatorValue("USER")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User  implements Serializable {
+
+    @Column(name = "USERNAME", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+
+    @Column(name = "FIRST_NAME", nullable = false)
+    private String firstName;
+
+    @Column(name = "LAST_NAME", nullable = false)
+    private String lastName;
+
+    @Column(name = "EMAIL", nullable = false, unique = true)
+    private String email;
+
+}
---
+++ src/main/java/com/kcodes/studygame/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.kcodes.studygame.repositories;
+
+import com.kcodes.studygame.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    User findByUsername(String username);
+}
